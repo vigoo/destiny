fa76b13fa25a9f25f06679d9b878de8ab909f6f2
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2025-03-17 09:36:08 +0100

    Initial store WIT

--- components-rust/destiny-store/src/lib.rs
+++ components-rust/destiny-store/src/lib.rs
@@ -1,20 +1,14 @@
+#[allow(static_mut_refs)]
 mod bindings;
 
 use crate::bindings::exports::destiny::store_exports::destiny_store_api::*;
-// Import for using common lib:
-// use common_lib::example_common_function;
 use std::cell::RefCell;
 
-/// This is one of any number of data types that our application
-/// uses. Golem will take care to persist all application state,
-/// whether that state is local to a function being executed or
-/// global across the entire program.
 struct State {
     total: u64,
 }
 
 thread_local! {
-    /// This holds the state of our application.
     static STATE: RefCell<State> = RefCell::new(State {
         total: 0,
     });
@@ -23,17 +17,44 @@ thread_local! {
 struct Component;
 
 impl Guest for Component {
-    /// Updates the component's state by adding the given value to the total.
-    fn add(value: u64) {
-        STATE.with_borrow_mut(|state| state.total += value);
+    fn add_destination(name: String, destination: UserDefinedDestination) {
+        todo!()
     }
 
-    /// Returns the current total.
-    fn get() -> u64 {
-        // Call code from shared lib
-        // println!("{}", example_common_function());
+    fn get_currency() -> Currency {
+        todo!()
+    }
 
-        STATE.with_borrow(|state| state.total)
+    fn get_destination(name: String) -> Destination {
+        todo!()
+    }
+
+    fn get_destinations() -> Vec<Destination> {
+        todo!()
+    }
+
+    fn get_home_location() -> String {
+        todo!()
+    }
+
+    fn get_ordered_destinations(preferences: Preferences) -> Vec<Destination> {
+        todo!()
+    }
+
+    fn remove_destination(name: String) {
+        todo!()
+    }
+
+    fn set_currency(currency: Currency) {
+        todo!()
+    }
+
+    fn set_home_location(location: String) {
+        todo!()
+    }
+
+    fn update_destination(name: String, destination: UserDefinedDestination) {
+        todo!()
     }
 }
 
--- journal.md
+++ journal.md
@@ -2,3 +2,10 @@
 
 Created the project and the first component (`destiny:store`) with golem cli.
 
+- Had to do a `git init` and added `.idea` to `.gitignore`
+- Renamed `common-lib` to `destiny-model` 
+- Defined an initial data model and API for the `store` in WIT
+- Changed rust edition to 2024
+- Added `#[allow(static_mut_refs)]` to the `mod bindings` to remove annoying warnings 
+- Straightforward store implementation with in-memory state
+- 
